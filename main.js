af679280e461f17ab58921a2360a2a43d7755e37
harld-x <danilyudin723@gmail.com>
2020-12-06 17:30:40 +0700

    environment platform

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const {app, BrowserWindow} = require('electron')
 
+
+process.env.NODE_ENV = 'development'
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
+
 let mainWindow;
 
 function createMainWindow () {
@@ -7,10 +13,25 @@ function createMainWindow () {
         title: 'ImageShrink',
         width: 500,
         height: 600,
-         icon: './assets/icons/Icon_256x256.png'
+         icon: './assets/icons/Icon_256x256.png',
+         resizable: isDev ? true : false,
     })
 
     mainWindow.loadFile('./app/index.html')
 }
 
 app.on('ready', createMainWindow)
+
+app.on('window-all-closed', () => {
+    if(isMac) {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0 ) {
+        createMainWindow()
+    }
+})
+
+app.allowRendererProcessReuse = true
