2d4e5913cde8ed13f8113cc4547e578fe48dbecd
harld-x <danilyudin723@gmail.com>
2020-12-06 21:36:33 +0700

    ipc communication

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, Menu} = require('electron')
+const {app, BrowserWindow, Menu, ipcMain} = require('electron')
 
 
 process.env.NODE_ENV = 'development'
@@ -93,6 +93,9 @@ const menu = [
     ] : [])
 ]
 
+ipcMain.on('image: minimize', (e, options) => {
+    console.log(options)
+})
 
 app.on('window-all-closed', () => {
     if(isMac) {
