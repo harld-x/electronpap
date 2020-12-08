8dc8a11b37182b6d106011e81a810e5ca3d3772a
harld-x <danilyudin723@gmail.com>
2020-12-08 21:54:55 +0700

    package, test app

--- main.js
+++ main.js
@@ -1,52 +1,50 @@
 const path = require('path')
 const os = require('os')
-const {app, BrowserWindow, Menu, ipcMain, shell} = require('electron')
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron')
 const imagemin = require('imagemin')
 const imageminMozjpeg = require('imagemin-mozjpeg')
 const imageminPngquant = require('imagemin-pngquant')
 const slash = require('slash')
+const log = require('electron-log')
 
-
-process.env.NODE_ENV = 'development'
+// Set env
+process.env.NODE_ENV = 'production'
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
 
-let mainWindow;
-let aboutWindow;
+let mainWindow
+let aboutWindow
 
-function createMainWindow () {
-     mainWindow = new BrowserWindow({
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
         title: 'ImageShrink',
         width: isDev ? 800 : 500,
         height: 600,
-         icon: './assets/icons/Icon_256x256.png',
-         resizable: isDev ? true : false,
-         backgroundColor: 'white',
-         webPreferences: {
-             nodeIntegration: true,
-         }
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+        resizable: isDev ? true : false,
+        backgroundColor: 'white',
+        webPreferences: {
+            nodeIntegration: true,
+        },
     })
 
-    if(isDev) {
+    if (isDev) {
         mainWindow.webContents.openDevTools()
     }
+
     mainWindow.loadFile('./app/index.html')
 }
 
-
-function createAboutWindow () {
+function createAboutWindow() {
     aboutWindow = new BrowserWindow({
         title: 'About ImageShrink',
         width: 300,
         height: 300,
-        icon: './assets/icons/Icon_256x256.png',
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
         resizable: false,
         backgroundColor: 'white',
-
     })
-
-
 
     aboutWindow.loadFile('./app/about.html')
 }
@@ -56,88 +54,93 @@ app.on('ready', () => {
 
     const mainMenu = Menu.buildFromTemplate(menu)
     Menu.setApplicationMenu(mainMenu)
-    mainWindow.on('ready', () => mainWindow = null)
 
+    mainWindow.on('ready', () => (mainWindow = null))
 })
 
 const menu = [
-    ...(isMac ? [
-        {
-            label: app.name,
-            submenu: [
-                {
-                    label: 'About',
-                    click: createAboutWindow,
-                }
-            ]
-        }
-    ]: []),
+    ...(isMac
+        ? [
+            {
+                label: app.name,
+                submenu: [
+                    {
+                        label: 'About',
+                        click: createAboutWindow,
+                    },
+                ],
+            },
+        ]
+        : []),
     {
         role: 'fileMenu',
     },
-    ...(!isMac ? [
-        {
-            label: 'Help',
-            submenu: [
-                {
-                    label: 'About',
-                    click: createAboutWindow,
-                }
-            ]
-        }
-    ]: []),
-    ...(isDev ? [
-        {
-            label: 'Developer',
-            submenu: [
-                { role: 'reload' },
-                { role: 'forcereload' },
-                { type: 'separator' },
-                { role: 'toggledevtools' },
-            ]
-        }
-    ] : [])
+    ...(!isMac
+        ? [
+            {
+                label: 'Help',
+                submenu: [
+                    {
+                        label: 'About',
+                        click: createAboutWindow,
+                    },
+                ],
+            },
+        ]
+        : []),
+    ...(isDev
+        ? [
+            {
+                label: 'Developer',
+                submenu: [
+                    { role: 'reload' },
+                    { role: 'forcereload' },
+                    { type: 'separator' },
+                    { role: 'toggledevtools' },
+                ],
+            },
+        ]
+        : []),
 ]
 
-ipcMain.on('image: minimize', (e, options) => {
+ipcMain.on('image:minimize', (e, options) => {
     options.dest = path.join(os.homedir(), 'imageshrink')
     shrinkImage(options)
 })
 
-
-async function shrinkImage({imgPath, quality, dest}) {
+async function shrinkImage({ imgPath, quality, dest }) {
     try {
-        const pngQuality = quality / 100;
+        const pngQuality = quality / 100
 
         const files = await imagemin([slash(imgPath)], {
             destination: dest,
             plugins: [
                 imageminMozjpeg({ quality }),
                 imageminPngquant({
-                    quality: [pngQuality, pngQuality]
-                })
-            ]
+                    quality: [pngQuality, pngQuality],
+                }),
+            ],
         })
 
-        console.log(files)
+        log.info(files)
 
-        shell.openItem(dest)
+//     Changed from shell.openItem() for v9
+        shell.openPath(dest)
 
         mainWindow.webContents.send('image:done')
-
     } catch (err) {
-        console.log(err)
+        log.error(err)
     }
 }
 
 app.on('window-all-closed', () => {
-    if(isMac) {
+    if (!isMac) {
         app.quit()
     }
 })
 
 app.on('activate', () => {
-    if(BrowserWindow.getAllWindows().length === 0 ) {
+    if (BrowserWindow.getAllWindows().length === 0) {
         createMainWindow()
     }
 })
